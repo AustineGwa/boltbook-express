69a0911d36c355070de8d7396eb27bd121c56e96
Clayton Ray <iamclaytonray@gmail.com>
2017-12-16 21:51:24 -0500

    Return more data for loggedInUser

--- src/loggedInUser.ts
+++ src/loggedInUser.ts
@@ -1,55 +1,67 @@
-import { fromEvent, FunctionEvent } from 'graphcool-lib'
-import { GraphQLClient } from 'graphql-request'
+import { fromEvent, FunctionEvent } from 'graphcool-lib';
+import { GraphQLClient } from 'graphql-request';
 
 interface User {
-  id: string
+  id: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  avatar: string;
 }
 
 export default async (event: FunctionEvent<{}>) => {
-  console.log(event)
 
   try {
     // no logged in user
     if (!event.context.auth || !event.context.auth.nodeId) {
-      return { data: null }
+      return { data: null };
     }
 
-    const userId = event.context.auth.nodeId
-    const graphcool = fromEvent(event)
-    const api = graphcool.api('simple/v1')
+    const userId = event.context.auth.nodeId;
+    const graphcool = fromEvent(event);
+    const api = graphcool.api('simple/v1');
 
     // get user by id
     const user: User = await getUser(api, userId)
-      .then(r => r.User)
+      .then((r) => r.User);
 
     // no logged in user
     if (!user || !user.id) {
-      return { data: null }
+      return { data: null };
     }
 
-    return { data: { id: user.id } }
+    return {
+      data: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.username,
+        avatar: user.avatar,
+      },
+    };
+
   } catch (e) {
-    console.log(e)
-    return { error: 'An unexpected error occured during authentication.' }
+    console.log(e);
+    return { error: 'An unexpected error occured during authentication.' };
   }
-}
+};
 
 async function getUser(api: GraphQLClient, id: string): Promise<{ User }> {
   const query = `
     query getUser($id: ID!) {
       User(id: $id) {
-        id,
-        firstName,
-        lastName,
-        username,
-        password
+        id
+        firstName
+        lastName
+        username
+        avatar
       }
     }
-  `
+  `;
 
   const variables = {
     id,
-  }
+  };
 
-  return api.request<{ User }>(query, variables)
+  return api.request<{ User }>(query, variables);
 }
